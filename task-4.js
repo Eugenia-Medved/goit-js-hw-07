043315342e637aba117936a9dc8cbaef3e67450f
YevheniiaBondarenko <65921369+YevheniiaBondarenko@users.noreply.github.com>
2020-10-14 22:03:02 +0300

    HW 7 task 4

---
+++ task-4.js
@@ -0,0 +1,11 @@
+const decrementBtn = document.querySelector(".decrement");
+const incrementBtn = document.querySelector(".increment");
+const counterValue = document.querySelector("#value");
+
+decrementBtn.addEventListener("click", () => {
+  counterValue.textContent -= 1;
+});
+
+incrementBtn.addEventListener("click", () => {
+  counterValue.textContent = parseInt(counterValue.textContent) + 1;
+});
